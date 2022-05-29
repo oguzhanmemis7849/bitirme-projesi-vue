450d6c630a6bd4feb85ab4e1e272edcbaaf2ec5a
Oguzhan <oguzhanmemis266@gmail.com>
2022-05-29 12:18:06 +0300

    linkedin github instagram linkleri koyuldu

--- src/views/About.vue
+++ src/views/About.vue
@@ -24,27 +24,67 @@
           <v-col cols="3"></v-col>
           <v-col cols="4">
               <v-card-text>
-                  Burak Bilikci
+                  <v-row>
+                      <v-col cols="12">
+                          Burak Bilikci
+                          <v-spacer></v-spacer>
+                          <a href="https://www.linkedin.com/in/burak-bilikci/"><v-icon>mdi-linkedin</v-icon></a>
+                          <a href="https://github.com/burakbilikci"><v-icon>mdi-github</v-icon></a>
+                          <a href="https://www.instagram.com/burakbilikci/"><v-icon>mdi-instagram</v-icon></a>
+                      </v-col>
+                  </v-row>
               </v-card-text>
           </v-col>
           <v-col cols="4">
               <v-card-text>
-                  Burak Köroğlu
+                  <v-row>
+                      <v-col cols="12">
+                          Burak Köroğlu
+                          <v-spacer></v-spacer>
+                          <a href="https://www.linkedin.com/in/burak-k%C3%B6ro%C4%9Flu-818984207/"><v-icon>mdi-linkedin</v-icon></a>
+                          <a href="https://github.com/korooglu"><v-icon>mdi-github</v-icon></a>
+                          <a href="https://www.instagram.com/kor.ooglu/"><v-icon>mdi-instagram</v-icon></a>
+                      </v-col>
+                  </v-row>
               </v-card-text>
           </v-col>
           <v-col cols="4">
               <v-card-text>
-                  Oğuzhan Memiş
+                  <v-row>
+                      <v-col cols="12">
+                          Oğuzhan Memiş
+                          <v-spacer></v-spacer>
+                          <a href="https://www.linkedin.com/in/o%C4%9Fuzhan-memi%C5%9F-3853b71b0/"><v-icon>mdi-linkedin</v-icon></a>
+                          <a href="https://github.com/oguzhanmemis7849  "><v-icon>mdi-github</v-icon></a>
+                          <a href="https://www.instagram.com/_oguzhanmemis/"><v-icon>mdi-instagram</v-icon></a>
+                      </v-col>
+                  </v-row>
               </v-card-text>
           </v-col>
           <v-col cols="6">
               <v-card-text>
-                  Tolga Kuruçay
+                  <v-row>
+                      <v-col cols="12">
+                          Tolga Kuruçay
+                          <v-spacer></v-spacer>
+                          <a href="https://www.linkedin.com/in/tolga-kuru%C3%A7ay-803b22208/"><v-icon>mdi-linkedin</v-icon></a>
+                          <a href="https://github.com/tolgakurucay"><v-icon>mdi-github</v-icon></a>
+                          <a href="https://www.instagram.com/tolgakurucay/"><v-icon>mdi-instagram</v-icon></a>
+                      </v-col>
+                  </v-row>
               </v-card-text>
           </v-col>
           <v-col cols="6">
               <v-card-text>
-                  Umut Teker
+                  <v-row>
+                      <v-col cols="12">
+                          Umut Teker
+                          <v-spacer></v-spacer>
+                          <a href="https://www.linkedin.com/in/umut-teker-104648216/"><v-icon>mdi-linkedin</v-icon></a>
+                          <a href="https://github.com/UmutTeker59"><v-icon>mdi-github</v-icon></a>
+                          <a href="https://www.instagram.com/umutteker1/"><v-icon>mdi-instagram</v-icon></a>
+                      </v-col>
+                  </v-row>
               </v-card-text>
           </v-col>
         </v-row>
@@ -109,5 +149,8 @@ export default {
 }
 .v-sheet.v-card:not(.v-sheet--outlined) {
     box-shadow: none !important;
+}
+a{
+    text-decoration: none;
 }
 </style>
