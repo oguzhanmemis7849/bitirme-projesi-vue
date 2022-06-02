f915bbe1d9109b638da11a67120779f6268200dc
Oguzhan <oguzhanmemis266@gmail.com>
2022-06-03 00:33:24 +0300

    kredi kartı eklendi

--- src/components/MyProfile/PaymentInfo.vue
+++ src/components/MyProfile/PaymentInfo.vue
@@ -1,55 +1,68 @@
 <template>
   <div>
-    <v-form ref="form" v-model="valid" lazy-validation>
+    <vue-paycard
+      :value-fields="valueFields"
+      :labels="labels"
+      :isCardNumberMasked="false"
+      :inputFields="inputFields"
+    />
+    <v-form class="pa-16" ref="form" v-model="valid" lazy-validation>
       <v-text-field
+        data-card-field
+        id="v-card-name"
         type="text"
-        v-model="cardOwnersName"
-        :rules="cardOwnersNameRules"
-        label="Kart Sahibi Adı"
+        v-model="valueFields.cardName"
+        label="Kart Sahibi Adı ve Soyadı"
         required
+        outlined
       ></v-text-field>
       <v-text-field
-        v-model="cardOwnersSurname"
-        :rules="nameRules"
-        label="Kart Sahibi Soyadı"
-        required
-      ></v-text-field>
-
-      <v-text-field
-        v-model="cardNo"
-        :rules="[rules.required, rules.counter]"
+        data-card-field
+        id="v-card-number"
+        v-model="valueFields.cardNumber"
         label="Kart numarası"
-        counter
-        maxlength="16"
+        type="number"
+        outlined
       ></v-text-field>
 
-      <v-text-field
-        v-model="cardDate"
+      <!-- <v-text-field
+        type="month"
+        v-model="valueFields.cardMonth"
         :rules="emailRules"
         label="Son Kullanma Tarihi"
         required
-      ></v-text-field>
+        outlined
+      ></v-text-field> -->
+
+      <div class="d-flex justif-center">
+        <v-select
+          data-card-field
+          id="v-card-month"
+          v-model="valueFields.cardMonth"
+          :items="months"
+          label="Ay"
+          outlined
+        ></v-select>
+        <v-select
+          data-card-field
+          id="v-card-year"
+          v-model="valueFields.cardYear"
+          :items="years"
+          label="Yıl"
+          outlined
+        ></v-select>
+      </div>
 
       <v-text-field
-        v-model="cardCVC"
-        :rules="emailRules"
-        label="CVC"
+        data-card-field
+        id="v-card-cvv"
+        v-model="valueFields.cardCvv"
+        label="CVV"
         required
+        outlined
       ></v-text-field>
 
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[(v) => !!v || 'Bilgilerinizin doğruluğunu kabul ediniz.']"
-        label="Kart bilgilerimin doğruluğunu kabul ediyorum."
-        required
-      ></v-checkbox>
-
-      <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="saveTheCard"
-      >
+      <v-btn color="success" class="mr-4" @click="saveTheCard">
         Kartı Kaydet
       </v-btn>
     </v-form>
@@ -57,10 +70,50 @@
 </template>
 
 <script>
+import { VuePaycard } from "vue-paycard";
 export default {
-  name: "PaymentInfo",
+  components: {
+    VuePaycard,
+  },
   data() {
     return {
+      months: [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+      ],
+      years: ["2022", "2023", "2024", "2024", "2025", "2026", "2027", "2028"], //şimdilik elle verildi dinamik yap
+      valueFields: {
+        cardName: "",
+        cardNumber: "",
+        cardMonth: "",
+        cardYear: "",
+        cardCvv: "",
+      },
+      labels: {
+        cardName: "Ad Soyad",
+        cardHolder: "Kart Sahibi",
+        cardMonth: "AY",
+        cardYear: "YY",
+        cardExpires: "SKT",
+        cardCvv: "CVV",
+      },
+      inputFields: {
+        cardNumber: "v-card-number",
+        cardName: "v-card-name",
+        cardMonth: "v-card-month",
+        cardYear: "v-card-year",
+        cardCvv: "v-card-cvv",
+      },
       rules: {
         required: (value) => !!value || "Bu alan zorunludur.",
         counter: (value) => value.length <= 16 || "En fazla 16 karakter",
